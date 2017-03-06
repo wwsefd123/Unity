cc6b49a7dd003d9a60dae525d7bd102878cae731
hansuKang <wwsefd123@sqisoft.com>
2017-03-06 17:59:55 +0900

    modify

--- Assets/Script/particleScript.cs
+++ Assets/Script/particleScript.cs
@@ -11,11 +11,13 @@ public class particleScript : MonoBehaviour {
 
     public GameObject particle;
     public List<GameObject> objList = new List<GameObject>();
-    private Vector3 mousePos;
-    private float inputX=50f;
-    private float inputY=50f;
+    //private Vector3 mousePos;
+
+    private Vector3[] inputXY;
+    private int inputCount = 0;
 
     Socket SeverSocket = null;
+    Socket client = null;
     Thread Socket_Thread = null;
     bool Socket_Thread_Flag = false;
 
@@ -41,27 +43,35 @@ public class particleScript : MonoBehaviour {
             float ranX = UnityEngine.Random.Range(-1.0f, 1.0f);
             float ranY = UnityEngine.Random.Range(-1.0f, 1.0f);
             obj.transform.Translate(new Vector3(ranX * speed * Time.deltaTime, ranY * speed * Time.deltaTime, 0));
-            if (Vector3.Distance(new Vector3(inputX, inputY, 0f), obj.transform.position) < 2f)
-            {
-                Vector3 pos = Vector3.MoveTowards(obj.transform.position, new Vector3(inputX, inputY, 0f), Time.deltaTime * speed);
-                obj.transform.position = pos;
 
-                
-            }else if(Vector3.Distance(mousePos, obj.transform.position) < 2f)
+            for(int i = 0; i<inputCount; i++)
             {
-                // 마우스이벤트를 받을때
-                if (Input.GetMouseButton(0))
+                if (Vector3.Distance(inputXY[i], obj.transform.position) < 2f)
                 {
-                    mousePos = Vector3.MoveTowards(obj.transform.position, mousePos, Time.deltaTime * speed);
-                    obj.transform.position = mousePos;
+                    Vector3 pos = Vector3.MoveTowards(obj.transform.position, inputXY[i], Time.deltaTime * speed);
+                    obj.transform.position = pos;
                 }
             }
-            
+            //if (Vector3.Distance(new Vector3(inputX[0], inputY[0], 0f), obj.transform.position) < 2f)
+            //{
+            //    Vector3 pos = Vector3.MoveTowards(obj.transform.position, new Vector3(inputX[0], inputY[0], 0f), Time.deltaTime * speed);
+            //    obj.transform.position = pos;
+            //}
+
+            //else if(Vector3.Distance(mousePos, obj.transform.position) < 2f)
+            //{
+            //    // 마우스이벤트를 받을때
+            //    if (Input.GetMouseButton(0))
+            //    {
+            //        mousePos = Vector3.MoveTowards(obj.transform.position, mousePos, Time.deltaTime * speed);
+            //        obj.transform.position = mousePos;
+            //    }
+            //}            
         });
-        if (Input.GetMouseButton(0))
-        {
-            mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
-        }
+        //if (Input.GetMouseButton(0))
+        //{
+        //    mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+        //}
     }
 
     IEnumerator CreatePositions()
@@ -81,6 +91,8 @@ public class particleScript : MonoBehaviour {
     {
         try
         {
+            client.Close();
+            SeverSocket.Close();
             Socket_Thread_Flag = false;
             Socket_Thread.Abort();
             SeverSocket.Close();
@@ -91,7 +103,7 @@ public class particleScript : MonoBehaviour {
             Debug.Log("소켓과 쓰레드 종료때 오류가 발생");
         }
     }
-
+    
     private void Dowrk()
     {
         SeverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -100,7 +112,7 @@ public class particleScript : MonoBehaviour {
         SeverSocket.Listen(10);
 
         Debug.Log("소켓 대기중....");
-        Socket client = SeverSocket.Accept();//client에서 수신을 요청하면 접속합니다.
+        client = SeverSocket.Accept();//client에서 수신을 요청하면 접속합니다.
         Debug.Log("소켓 연결되었습니다.");
 
         IPEndPoint clientep = (IPEndPoint)client.RemoteEndPoint;
@@ -114,11 +126,18 @@ public class particleScript : MonoBehaviour {
             try
             {
                 recvStm.Read(receiveBuffer, 0, receiveBuffer.Length);
-                string Test = Encoding.Default.GetString(receiveBuffer);
+                string[] Test = Encoding.Default.GetString(receiveBuffer).Split('E');
                 Debug.Log(Test);
-                string[] xylocation = Test.Split(',');
-                inputX =13f -  System.Convert.ToSingle(xylocation[0])/486*13f;
-                inputY = -(10f - System.Convert.ToSingle(xylocation[1])/1800*10f);
+                inputXY = new Vector3[50];
+                for (int i = 0; i < Test.Length - 1; i++)
+                {
+                    string[] xylocation = Test[i].Split(',');
+                    inputXY[i] = new Vector3(System.Convert.ToSingle(xylocation[0]) * 13f, -(System.Convert.ToSingle(xylocation[1]) * 10f), 0f);
+                    //inputXY[i] = new Vector3(13f - System.Convert.ToSingle(xylocation[0]) / 486 * 13f, -(10f - System.Convert.ToSingle(xylocation[1]) / 1800 * 10f), 0f);
+                    inputCount = i + 1;
+                    Debug.Log("REC-INPUT ["+ i +"] ( X , Y ) = ( " + inputXY[i].x + " , " + inputXY[i].y + " )");
+                    Debug.Log("input Count = " + inputCount);
+                }             
             }
 
             catch (Exception e)
