e4725aa3b7b903007621184eb5215f4058488a54
hansuKang <wwsefd123@sqisoft.com>
2017-03-09 17:58:12 +0900

    수정반영

--- Assets/Script/particleScript.cs
+++ Assets/Script/particleScript.cs
@@ -14,9 +14,9 @@ public class particleScript : MonoBehaviour {
     // GameObject(mold)
     public GameObject particle;
     private List<GameObject> objList = new List<GameObject>();
-    public int maxObject = 20;
-    public float screenWidth = 13.3f;
-    public float screenHeight = 10f;
+    public int maxObject = 50;
+    public float screenWidth = 1333f;
+    public float screenHeight = 1000f;
 
     //Coordinates received from Kinect
     private Vector3[] inputXY;
@@ -46,7 +46,7 @@ public class particleScript : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        float speed = 3f;
+        float speed = 300f;
         objList.ForEach(delegate (GameObject obj)
         {
             //=======================================================
@@ -59,7 +59,7 @@ public class particleScript : MonoBehaviour {
             // 키넥트로 받은 좌표 처리
             for (int i = 0; i<inputCount; i++)
             {
-                if (Vector3.Distance(inputXY[i], obj.transform.position) < 2f) // 키넥트 좌표와 거리가 200px 이하일 경우
+                if (Vector3.Distance(inputXY[i], obj.transform.position) < 200f) // 키넥트 좌표와 거리가 200px 이하일 경우
                 {
                     Vector3 pos = Vector3.MoveTowards(obj.transform.position, inputXY[i], Time.deltaTime * speed); // 끌어당김
                     obj.transform.position = pos;
@@ -67,7 +67,7 @@ public class particleScript : MonoBehaviour {
             }
             //=======================================================
             // 마우스이벤트로 받은 좌표 처리
-            if (Vector3.Distance(mousePos, obj.transform.position) < 2f) // 마우스 좌표와 거리가 200px 이하일 경우
+            if (Vector3.Distance(mousePos, obj.transform.position) < 200f) // 마우스 좌표와 거리가 200px 이하일 경우
             {
                 // 마우스이벤트를 받을때
                 if (Input.GetMouseButton(0))
@@ -90,8 +90,8 @@ public class particleScript : MonoBehaviour {
     // 2초마다 랜덤 오브젝트 생성, 연결 끊긴경우 서버소켓 재실행
     IEnumerator CreatePositions()
     {
-        float randX = UnityEngine.Random.Range(0, 13.0f);
-        float randY = UnityEngine.Random.Range(-10.0f, 0);
+        float randX = UnityEngine.Random.Range(0, 1333f);
+        float randY = UnityEngine.Random.Range(-1000f, 0);
         if(objList.Count < maxObject)  // 최대 오브젝트 생성 갯수
         {
             GameObject mold = (GameObject)Instantiate(particle, new Vector3(randX, randY, 0f), Quaternion.identity);
@@ -104,7 +104,7 @@ public class particleScript : MonoBehaviour {
             Socket_Thread_Flag = true;
             Socket_Thread.Start();
         }
-        yield return new WaitForSeconds(2);// 2초마다
+        yield return new WaitForSeconds(1);// 2초마다
         StartCoroutine("CreatePositions");
     }
 
@@ -134,9 +134,9 @@ public class particleScript : MonoBehaviour {
         SeverSocket.Bind(ipep);
         SeverSocket.Listen(10);
 
-        Debug.Log("소켓 대기중....");
+        Debug.Log ("소켓 대기중....");
         client = SeverSocket.Accept();//client에서 수신을 요청하면 접속합니다.
-        Debug.Log("소켓 연결되었습니다.");
+        Debug.Log ("소켓 연결되었습니다.");
 
         IPEndPoint clientep = (IPEndPoint)client.RemoteEndPoint;
         NetworkStream recvStm = new NetworkStream(client);
@@ -154,7 +154,7 @@ public class particleScript : MonoBehaviour {
                 // 클라이언트 소켓접속이 끊어진경우
                 if (readbytes == 0) 
                 {                    
-                    Debug.Log("소켓 접속 끊어짐.");
+                    Debug.Log ("소켓 접속 끊어짐.");
                     Socket_Thread_Flag = false;
                     client.Close();
                     SeverSocket.Close();
@@ -168,11 +168,11 @@ public class particleScript : MonoBehaviour {
                 inputXY = new Vector3[50];
                 for (int i = 0; i < recvString.Length - 1; i++)
                 {
-                    string[] xylocation = recvString[i].Split(','); // 받는 좌표값  = X , Y 가 0~1 사이의 화면에 있는 비율값임 ex> 정가운데 = 0.5,0.5
-                    inputXY[i] = new Vector3(System.Convert.ToSingle(xylocation[0]) * screenWidth, -(System.Convert.ToSingle(xylocation[1]) * screenHeight), 0f); // y값은 -를 해줘야함
+                    string[] xylocation = recvString[i].Split(','); 
+                    inputXY[i] = new Vector3(System.Convert.ToSingle(xylocation[0]) , -(System.Convert.ToSingle(xylocation[1]) ), 0f); // y값은 -를 해줘야함
                     inputCount = i + 1;
-                    Debug.Log("REC-INPUT ["+ i +"] ( X , Y ) = ( " + inputXY[i].x + " , " + inputXY[i].y + " )");
-                    Debug.Log("input Count = " + inputCount);
+                    Debug.Log ("REC-INPUT ["+ i +"] ( X , Y ) = ( " + inputXY[i].x + " , " + inputXY[i].y + " )");
+                    Debug.Log ("input Count = " + inputCount);
                 }             
             }
 
